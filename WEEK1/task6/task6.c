9b4d86453109b38444a3b264ad66cd3f5f871518
IslamNagi14 <152648172+IslamNagi14@users.noreply.github.com>
2024-02-05 00:12:40 +0200

    Update task6.c

--- WEEK1/task6/task6.c
+++ WEEK1/task6/task6.c
@@ -6,11 +6,16 @@ void main(void)
 	scanf("%d",&num1);
 	printf("enter the 2nd number : ");
 	scanf("%d",&num2);
+	
 	printf("%d + %d = %d\n",num1,num2,num1 + num2);
 	printf("%d - %d = %d\n",num1,num2,num1 - num2);
 	printf("%d * %d = %d\n",num1,num2,num1 * num2);
-	printf("%d / %d = %d\n",num1,num2,num1 / num2);
-	printf("%d %% %d = %d\n",num1,num2,num1 % num2);
+	if(num2 == 0)
+	{printf(" the 2nd number  is not valid to make division operation\n");} 
+	else{
+		printf("%d / %d = %d\n",num1,num2,num1 / num2);
+		printf("%d %% %d = %d\n",num1,num2,num1 % num2);
+	    }
 	printf("%d & %d = %d\n",num1,num2,num1 & num2);
 	printf("%d | %d = %d\n",num1,num2,num1 | num2);
 	printf("%d ^ %d = %d\n",num1,num2,num1 ^ num2);
