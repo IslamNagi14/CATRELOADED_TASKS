2ab7190e56f5fb48f9953f447d8fb1d45dba2d3a
IslamNagi14 <152648172+IslamNagi14@users.noreply.github.com>
2024-02-06 22:39:56 +0200

    Update task10.c

--- WEEK2/task10/task10.c
+++ WEEK2/task10/task10.c
@@ -1,28 +1,31 @@
 #include<stdio.h>
 void main(void)
 {
-	int id;
-	
-	
-	for(int i = 1; i <= 3 ; i++)
-	{
-	printf("your %d attempt\n",i);
+	int id,pas; // id-->12300...pas-->12012
 	printf("enter your ID : ");
 	scanf("%d",&id);
-	if( id != 12300)
+	if( id == 12300)
 	{
-			printf("You are nor registed\n");
-		if(i==3)
+		for(int i = 1; i <= 3 ; i++)
+	    {
+	      printf("please,enter your password : ");
+	      scanf("%d",&pas);
+	      if(pas == 12012)
+	      {
+		    printf("Welcome Sir...");
+		    break;
+	      }
+	      else
+	      {
+			   if(i<3)
+		    printf("You are nor registed\nTry Again\n");
+		   else
 			printf("No more tries");
+	      }
+			
+	    }
 	}
 	else
-	{
-		printf("Welcome Sir...");
-		break;
-	
-	}
-	
-	
-	}
+		printf("Wrong id ,try next time ");
 	
 }
